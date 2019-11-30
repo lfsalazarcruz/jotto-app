19ba848f6315e1ab125d94bbcbc3c8d0c86cd7ed
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-11-29 21:54:06 -0800

    Initial Jotto app setup.

---
+++ client/src/Components/Congrats.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @returns {JSX.Element} - Rendered component (or null if 'success' prop is )
+ */
+
+export default () => {
+  return <div>Congrats</div>;
+};
---
+++ client/src/Components/Congrats.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+
+import Congrats from "./Congrats";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+/**
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @return {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
+
+test("renders without error", () => {});
+test("renders no text when `succes` prop is false", () => {});
+test("renders non-empty congrats message when success prop is true", () => {});
