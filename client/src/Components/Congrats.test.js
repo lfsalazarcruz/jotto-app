052096a5ccdc4c759dc563da14a39b08a279746a
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-11-29 22:42:26 -0800

    All test passing.

--- client/src/Components/Congrats.test.js
+++ client/src/Components/Congrats.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 
-import { findByTestAttr } from "../../test/utilities";
+import { findByTestAttr, checkProps } from "../../test/utilities";
 import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
@@ -17,7 +17,7 @@ const setup = (props = {}) => {
 };
 
 test("renders without error", () => {
-  const wrapper = setup();
+  const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
@@ -30,4 +30,8 @@ test("renders non-empty congrats message when success prop is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
+});
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
