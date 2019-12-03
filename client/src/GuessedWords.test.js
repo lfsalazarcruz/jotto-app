308f7ed01259629f7541b6e954e12771447b6259
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-12-02 23:26:44 -0800

    Initial tests for GuessedWords component

--- client/src/GuessedWords.test.js
+++ client/src/GuessedWords.test.js
@@ -1,4 +1,26 @@
-import React from 'react';
-import {shallow} from Enzyme;
-import { findByTestAttr, checkProps } from '../test/utilities';
-import GuessedWords from './GuessedWords';
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, checkProps } from "../test/utilities";
+import GuessedWords from "./GuessedWords";
+
+const defaultProps = {
+  guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }]
+};
+/**
+ * Factory function to create a ShallowWrapper for the GuessedWords component
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<GuessedWords {...setupProps} />);
+};
+
+test("does not throw warning with expected props", () => {
+  checkProps(GuessedWords, defaultProps);
+});
+
+describe("if there are no words guessed", () => {});
+
+describe("if there are words guessed", () => {});
